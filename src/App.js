7056aef11ae4b72bb8e7cfa34b29ad37a23fdc76
Robert Austin <austin437@hotmail.com>
2022-03-17 07:52:59 +0000

    Updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
 import React from "react";
-import Navigation from "./components/Navigation";
-import { Outlet } from "react-router-dom";
 
 class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <Navigation />
-                <Outlet />
                 <p>And here is some basic menu information</p>
             </div>
         );
