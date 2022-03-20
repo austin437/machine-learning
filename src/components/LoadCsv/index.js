7015665786102682ee5d7af7b5b2b917f6af7014
Robert Austin <austin437@hotmail.com>
2022-03-20 10:04:28 +0000

    LoadCSV: created reducer

--- src/components/LoadCsv/index.js
+++ src/components/LoadCsv/index.js
@@ -1,45 +1,61 @@
-import React, { useState } from "react";
+import React, { useReducer, useEffect, useCallback } from "react";
 import { Button, Input } from "@mui/material";
 import classes from "./styles.module.css";
 import { loadCSV as LoadCsvHelper } from "../../helpers";
 
-const LoadCsv = (props) => {
-    const [fileInput, setfileInput] = useState(null);
-    const [fileData, setFileData] = useState("{}");
+const initialState = { fileInput: null, fileData: "{}" };
 
-    const readDataFromFile = () => {
-        const reader = new FileReader();
-        reader.onload = function (event) {
-            const str = event.target.result;
-            const rows = str.split("\n", 15);
-            setFileData(rows.join("\n") + "\n...");
-        };
-        reader.readAsText(fileInput);
-    };
+function reducer(state, action) {
+    switch (action.type) {
+        case "setFileInput":
+            return { ...state, fileInput: action.payload };
+        case "setFileData":
+            return { ...state, fileData: action.payload };
+        default:
+            throw new Error();
+    }
+}
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+function useActions(state, dispatch) {
+    const readDataFromFile = useCallback(() => {
+        if (state.fileInput) {
+            const reader = new FileReader();
+            reader.onload = function (event) {
+                const str = event.target.result;
+                const rows = str.split("\n", 15);
+                dispatch({ type: "setFileData", payload: rows.join("\n") + "\n..." });
+            };
+            reader.readAsText(state.fileInput);
+        }
+    }, [state.fileInput]);
+
+    useEffect(() => {
         readDataFromFile();
+    }, [readDataFromFile]);
+
+    const handleFileChange = (event) => {
+        dispatch({ type: "setFileInput", payload: event.target.files[0] });
     };
 
-    const handleChange = (event) => {
-        setfileInput(event.target.files[0]);
-    };
+    return { handleFileChange };
+}
+
+const LoadCsv = (props) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const { handleFileChange } = useActions(state, dispatch);
+
     return (
         <>
             <div className={classes.callout}>
                 <p>Please select a CSV file</p>
             </div>
-            <form onSubmit={handleSubmit}>
+            <form>
                 <label>
                     Upload CSV:
-                    <Input className={classes.fileInput} type="file" onChange={handleChange} name="csvFile" />
+                    <Input className={classes.fileInput} type="file" onChange={handleFileChange} name="csvFile" />
                 </label>
-                <Button disabled={fileInput === null} variant="contained" type="submit">
-                    Submit
-                </Button>
             </form>
-            <pre className={classes.pre}>{fileData}</pre>
+            <pre className={classes.pre}>{state.fileData}</pre>
         </>
     );
 };
