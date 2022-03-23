0fbca8b75639c8004e47314fd4e4fb78d2da6cda
Robert Austin <austin437@hotmail.com>
2022-03-23 18:23:41 +0000

    FeatureSelector: used mui tables

--- src/components/FeatureSelector/index.jsx
+++ src/components/FeatureSelector/index.jsx
@@ -1,25 +1,27 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import classes from "./styles.module.css";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Checkbox from "@mui/material/Checkbox";
+import Button from "@mui/material/Button";
 
 const FeatureSelector = ({ data, ...props }) => {
-    const [isGoing, setIsGoing] = useState(true);
-    const [numberOfGuests, setNumberOfGuests] = useState(2);
+    const [checked, setChecked] = React.useState([]);
 
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log(event.target.elements);
     };
 
-    const handleInputChange = (event) => {
-        setIsGoing(event.target.checked);
-    };
+    const handleInputChange = (event) => {};
 
-    const handleGuestChange = (event) => {
-        setNumberOfGuests(event.target.value);
-    };
-
-    const formTestData = [
+    const rows = [
         { fieldName: "MPG", dataType: "number" },
         { fieldName: "HorsePower", dataType: "number" },
         { fieldName: "Weight", dataType: "string" },
@@ -31,32 +33,35 @@ const FeatureSelector = ({ data, ...props }) => {
             <pre>{data.headers.map((v) => v + "\n")}</pre>
 
             <form className={classes.form} onSubmit={handleSubmit}>
-                <table className={classes.table}>
-                    <thead>
-                        <tr>
-                            <th>Feature</th>
-                            <th>Datatype</th>
-                            <th>Use?</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {formTestData.map((v) => (
-                            <tr key={v.fieldName}>
-                                <td>{v.fieldName}</td>
-                                <td>{v.dataType}</td>
-                                <td>
-                                    <input
-                                        name="isGoing"
-                                        type="checkbox"
-                                        checked={isGoing}
-                                        onChange={handleInputChange}
-                                    />
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-                <input type="submit" value="Submit" />
+                <TableContainer sx={{ marginBottom: 2 }} component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Feature</TableCell>
+                                <TableCell>Data type</TableCell>
+                                <TableCell align="center">Use</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map((row) => (
+                                <TableRow
+                                    key={row.fieldName}
+                                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                                >
+                                    <TableCell>{row.fieldName}</TableCell>
+                                    <TableCell sx={{ textTransform: "capitalize" }}>{row.dataType}</TableCell>
+                                    <TableCell align="center">
+                                        <Checkbox onChange={handleInputChange} />
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+
+                <Button type="submit" variant="contained">
+                    Submit
+                </Button>
             </form>
         </>
     );
